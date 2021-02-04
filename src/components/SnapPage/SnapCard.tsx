804796be8a8115a5310b05705273395e375d486b
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2021-02-04 15:19:47 +0900

    210204 snap antd sidebar menu < LeftMenu

--- src/components/SnapPage/SnapCard.tsx
+++ src/components/SnapPage/SnapCard.tsx
@@ -1,80 +1,44 @@
-import React from 'react';
+import { Layout, Menu } from 'antd';
 import styled from 'styled-components';
-import { Row, Col } from 'antd';
 
-// <SnapCardContainer>
-//     <img src="http://via.placeholder.com/300.png/09f/fff" alt="snapImage" />
-{
-    /* <Row>
-                <Col
-                    xs={{ span: 5, offset: 1 }}
-                    sm={{ span: 6, offset: 2 }}
-                    md={{ span: 6, offset: 2 }}
-                    lg={{ span: 6, offset: 2 }}
-                    xl={{ span: 6, offset: 2 }}
-                    xxl={{ span: 6, offset: 2 }}
-                >
-                    <img src="http://placehold.it/240x240" alt="lookbookImage" />
-                </Col>
-            </Row> */
-}
-
-const SnapCard = (props: any) => {
+const SnapCard = () => {
     return (
-        <SnapCardContainer>
-            <SnapCardGrid>
-                <SnapCardPadded>
-                    <Centered src="http://via.placeholder.com/816x481" />
-                </SnapCardPadded>
-                <SnapCardRendered>
-                    <Centered src="http://via.placeholder.com/816x481" />
-                </SnapCardRendered>
-
-                {/* <SnapCardPadded>
-                    <Centered src="http://via.placeholder.com/816x481" />
-                </SnapCardPadded>
-
-                <SnapCardPadded>
-                    <Centered src="http://via.placeholder.com/604x611" />
-                </SnapCardPadded>
-
-                <SnapCardPadded>
-                    <Centered src="http://via.placeholder.com/940x667" />
-                </SnapCardPadded> */}
-            </SnapCardGrid>
-        </SnapCardContainer>
+        <SnapCardGrid>
+            <SnapCardRendered>
+                <Centered src="http://via.placeholder.com/816x481" />
+            </SnapCardRendered>
+        </SnapCardGrid>
     );
 };
 
-const SnapCardContainer = styled.div`
-    cursor: pointer;
-    color: ${({ theme }) => theme.mode.textColor};
-`;
+export default SnapCard;
 
 const SnapCardGrid = styled.div`
     display: flex;
-    justify-content: space-between;
+    /* justify-content: space-between; */
     flex-wrap: wrap;
 `;
 
 const SnapCardWrapper = styled.div`
     display: inline-block;
-    width: calc(100% / 3);
-    margin: 2px;
+    /* width: calc(100% / 3); */
+    /* margin: 100px; */
     border: 1px solid red;
 `;
 
-const SnapCardPadded = styled(SnapCardWrapper)`
-    height: 0;
-    position: relative;
-    overflow: hidden;
-    padding-bottom: 56.25% / 4;
-`;
+// const SnapCardPadded = styled(SnapCardWrapper)`
+//     height: 0;
+//     position: relative;
+//     overflow: hidden;
+//     padding-bottom: 56.25% / 4;
+// `;
 
-const SnapCardRendered = styled(SnapCardWrapper)`
+const SnapCardRendered = styled.div`
+    display: inline-block;
+    margin: 50px;
     margin-left: auto;
     width: calc(200% / 3) !important;
-    height: 0;
+    height: auto;
     position: relative;
     overflow: hidden;
     padding-bottom: 56.25% / 4;
@@ -94,5 +58,3 @@ const Centered = styled(SnapCardImage)`
     bottom: -50%;
     margin: auto;
 `;
-
-export default SnapCard;
--- src/pages/snapIndex.tsx
+++ src/pages/snapIndex.tsx
@@ -3,7 +3,11 @@ import NavLayout from '../components/NavLayout';
 import LeftMenu from '../components/LeftMenu';
 import styled from 'styled-components';
 import SnapCard from '../components/SnapPage/SnapCard';
-import { Row, Col, List } from 'antd';
+import { Row, Col, List, Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 const snaps = [
     { id: 1, title: 'title1', source: 'blue' },
@@ -17,37 +21,97 @@ const snaps = [
     { id: 9, title: 'title9', source: 'gray' },
 ];
 
-export default function SnapIndex() {
+export default function SnapIndex(props: any) {
     return (
         <NavLayout>
-            {/* <Row style={{ marginBottom: '100px' }}>
-                <Col xs={24} sm={24} md={24} lg={8}>
-                    <LeftMenu />
-                </Col>
-
-                <Col xs={24} sm={24} md={24} lg={14} style={{ margin: '98px 0px 100px 20px' }}>
-                    <List
-                        grid={{
-                            xs: 1,
-                            sm: 2,
-                            md: 3,
-                            lg: 3,
-                            xl: 3,
-                            xxl: 3,
-                        }}
-                        dataSource={snaps}
-                        renderItem={(item) => (
-                            <List.Item style={{ marginBottom: '-10px' }}>
-                                <SnapCard key={item.id} {...item} />
-                            </List.Item>
-                        )}
-                    />
-                </Col>
-            </Row> */}
-            <LeftMenu />
-            {snaps.map((snap) => (
-                <SnapCard key={snap.id} {...snap} />
-            ))}
+            {/* <LeftMenu /> */}
+            <Layout>
+                <Sider
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onBreakpoint={(broken) => {
+                        console.log(broken);
+                    }}
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                    style={{ backgroundColor: 'white' }}
+                    width="600"
+                >
+                    <div className="logo" />
+                    <Menu theme="light" defaultSelectedKeys={['1']} style={{ margin: '0px 0px 0px 100px ' }}>
+                        <MenuTitleWrapper>
+                            <Link href="/snapIndex">
+                                <MenuIndexTitle style={{ margin: '0px 0px 145px 0px' }}>
+                                    <Text>SNAP</Text>
+                                    <TitleBorder></TitleBorder>
+                                </MenuIndexTitle>
+                            </Link>
+                            <Link href="/lookbookIndex">
+                                <MenuIndexTitle style={{ margin: '0px 0px 145px 0px' }}>
+                                    <Text>LOOKBOOK</Text>
+                                    <TitleBorderLookBook></TitleBorderLookBook>
+                                </MenuIndexTitle>
+                            </Link>
+                            <Link href="/jobIndex">
+                                <MenuIndexTitle>
+                                    <Text>JOB</Text>
+                                    <TitleBorderJob></TitleBorderJob>
+                                </MenuIndexTitle>
+                            </Link>
+                        </MenuTitleWrapper>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ padding: 0, backgroundColor: 'white' }} />
+                    <Content style={{ margin: '24px 16px 0' }}>
+                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                            {snaps.map((snap) => (
+                                <SnapCard key={snap.id} {...snap} />
+                            ))}
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
         </NavLayout>
     );
 }
+
+const MenuTitleWrapper = styled.div`
+    margin: 150px 0 0 0;
+    font-family: 'Poppins', sans-serif;
+    font-size: 64px;
+    font-weight: 800;
+`;
+
+const MenuIndexTitle = styled.h2`
+    position: relative;
+    margin: 0 0 80px 0;
+    color: ${({ theme }) => theme.mode.textColor};
+    cursor: pointer;
+`;
+
+const TitleBorder = styled.span`
+    border-bottom: 8px solid ${({ theme }) => theme.mode.borderColor};
+    margin: 2px 0 0 0;
+    width: 178px;
+    position: absolute;
+    top: 32px;
+    right: 260px;
+`;
+
+const TitleBorderLookBook = styled(TitleBorder)`
+    width: 272px;
+    top: 30px;
+    right: 78px;
+`;
+
+const TitleBorderJob = styled(TitleBorder)`
+    width: 99px;
+    right: 330px;
+`;
+
+const Text = styled.h2`
+    color: ${({ theme }) => theme.mode.textColor};
+    right: 130px;
+`;
