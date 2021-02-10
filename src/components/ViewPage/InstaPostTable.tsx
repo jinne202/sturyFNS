e635c01db532d909e225de913ec61af8ac7e237d
BonhaengLee <59217352+BonhaengLee@users.noreply.github.com>
2021-02-10 23:39:02 +0900

    210210 viewInsta done

--- src/components/ViewPage/InstaPostTable.tsx
+++ src/components/ViewPage/InstaPostTable.tsx
@@ -1,69 +1,66 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const arrowIcon = (
-    <svg width="20" height="20" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M25.7751 17.7501H0.25V14.2501H25.7751L14.7626 3.23757L17.2374 0.762695L32.4749 16.0001L17.2374 31.2376L14.7626 28.7627L25.7751 17.7501Z"
-        />
-    </svg>
-);
+import Link from 'next/link';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 
 export interface InstaPostTableProps {}
 
 const InstaPostTable: React.SFC<InstaPostTableProps> = (props: any) => {
     return (
-        <ApplyListContainer>
-            <ApplyTitle>{props.title}</ApplyTitle>
-            <ApplyBrand>
-                {props.brand}
-                <Icon>{arrowIcon}</Icon>
-            </ApplyBrand>
-        </ApplyListContainer>
+        <InstaPostTableContainer>
+            <button>
+                <AiOutlineArrowLeft />
+            </button>
+            <PostWrapper>
+                <Link href={`/instagram/${1}`}>
+                    <a>
+                        <img src="http://placehold.it/240x240" alt="snapImage" />
+                    </a>
+                </Link>
+                <Link href={`/instagram/${1}`}>
+                    <a>
+                        <img src="http://placehold.it/240x240" alt="snapImage" />
+                    </a>
+                </Link>
+                <Link href={`/instagram/${1}`}>
+                    <a>
+                        <img src="http://placehold.it/240x240" alt="snapImage" />
+                    </a>
+                </Link>
+                <Link href={`/instagram/${1}`}>
+                    <a>
+                        <img src="http://placehold.it/240x240" alt="snapImage" />
+                    </a>
+                </Link>
+            </PostWrapper>
+            <button>
+                <AiOutlineArrowRight />
+            </button>
+        </InstaPostTableContainer>
     );
 };
 
-const Icon = styled.div`
-    position: absolute;
-    right: -40px;
-    top: 18px;
-    transition: 0.5s ease all;
-    -moz-transition: 0.5s ease all;
-    -webkit-transition: 0.5s ease all;
-    opacity: 0;
+const InstaPostTableContainer = styled.div`
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    color: ${({ theme }) => theme.mode.textColor};
 
-    & > svg > path {
-        fill: ${({ theme }) => theme.mode.borderColor};
+    & > button {
+        margin-top: auto;
+        margin-bottom: auto;
+        height: 50px;
     }
 `;
 
-const ApplyTitle = styled.p`
-    padding: 18px 0 0 0;
-    margin: 0 0 0 70px;
-    width: 65%;
-    color: ${({ theme }) => theme.mode.textColor};
-`;
-
-const ApplyBrand = styled.p`
-    padding: 18px 0 0 0;
-    color: ${({ theme }) => theme.mode.textColor};
-    position: relative;
-`;
-
-const ApplyListContainer = styled.div`
-    border-bottom: 4px solid ${({ theme }) => theme.mode.borderColor};
-    font-size: 18px;
-    font-weight: 700;
-    display: flex;
-    height: 60px;
-    cursor: pointer;
-
-    &:hover {
-        & > ${ApplyBrand} > ${Icon} {
-            opacity: 100%;
-        }
+const PostWrapper = styled(InstaPostTableContainer)`
+    display: grid;
+    grid-template-columns: repeat(4, 0fr);
+    justify-content: center;
+    padding: 0 40px 0 40px;
+    & > a > img {
+        width: 100%/4;
+        height: 100%;
     }
 `;
 
--- src/components/ViewPage/ViewInsta.tsx
+++ src/components/ViewPage/ViewInsta.tsx
@@ -4,20 +4,12 @@ import InstaPostTable from './InstaPostTable';
 
 const dummyUserImg = require('../../../static/userImage.png');
 
-const MyApplys = [
-    { id: 1, title: '공고 제목 1', brand: '18명 지원중' },
-    { id: 2, title: '공고 제목 2', brand: '3명 지원중' },
-    { id: 3, title: '공고 제목 3', brand: '5명 지원중' },
-    { id: 4, title: '공고 제목 4', brand: '21명 지원중' },
-];
-
 export interface ViewInstaProps {}
 
 const ViewInsta: React.SFC<ViewInstaProps> = (props: any) => {
     return (
         <UserMyPageWrapper>
             <MyCard>
-                {/* <LeftTypeTitle>Model</LeftTypeTitle> */}
                 <RightWrapper>
                     <UserImg>
                         <img src={dummyUserImg} alt="더미 유저 이미지" />
@@ -26,9 +18,7 @@ const ViewInsta: React.SFC<ViewInstaProps> = (props: any) => {
                 </RightWrapper>
             </MyCard>
             <MyInfo>
-                {MyApplys.map((apply) => (
-                    <InstaPostTable key={apply.id} {...apply} />
-                ))}
+                <InstaPostTable />
             </MyInfo>
             <ButtonWrapper>
                 <LoginButton type="submit">모델에게 문의하기</LoginButton>
@@ -72,7 +62,6 @@ const UserName = styled.p`
     font-weight: bold;
     line-height: 155px;
     text-align: left;
-    /* clear: both; */
     color: ${({ theme }) => theme.mode.textColor};
 `;
 
@@ -82,7 +71,6 @@ const MyInfo = styled.div`
 
 const ButtonWrapper = styled.div`
     display: flex;
-    /* height: 100vh; */
     justify-content: center;
     align-items: center;
     padding: 0 0 100px 0;
